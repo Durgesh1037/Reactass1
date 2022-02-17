318e2efb7a896ca5be6afc5efbde4e45acd8a43e
Durgesh1037 <94463075+Durgesh1037@users.noreply.github.com>
2022-02-17 05:30:29 -0800

    first

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+// export default App;
